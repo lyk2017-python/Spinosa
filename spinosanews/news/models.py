d78a34b4eb110d34b46b95703f6db894f1ce42ec
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-07-25 17:35:25 +0300

    models.py modifed (news, comment, category), requierements modified (pillow).

--- spinosanews/news/models.py
+++ spinosanews/news/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    source = models.URLField()
+    slug = models.SlugField()
+    image = models.ImageField(null=True, blank=True)
+    publish_date = models.DateField(default=timezone.now)
+    update_date = models.DateField(auto_now=True)
+    likes = models.SmallIntegerField(default=0)
+    reports = models.PositiveSmallIntegerField(default=0)
+    categories = models.ManyToManyField("Category")
+
+class Comment(models.Model):
+    content = models.CharField(max_length=255)
+    news = models.ForeignKey("News")
+    publish_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+    likes = models.SmallIntegerField(default=0)
+    reports = models.PositiveSmallIntegerField(default=0)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
