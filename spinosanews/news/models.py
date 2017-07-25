229fd9d986afe9fb45885b56ca2a7bc0306bdd6d
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-07-25 21:15:23 +0300

    Models str methods defined.

--- spinosanews/news/models.py
+++ spinosanews/news/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class News(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField(unique=True)
     source = models.URLField()
     slug = models.SlugField()
     image = models.ImageField(null=True, blank=True)
@@ -14,6 +14,9 @@ class News(models.Model):
     reports = models.PositiveSmallIntegerField(default=0)
     categories = models.ManyToManyField("Category")
 
+    def __str__(self):
+        return "{id}-{title}".format(id = self.id, title = self.title[0:20])
+
 class Comment(models.Model):
     content = models.CharField(max_length=255)
     news = models.ForeignKey("News")
@@ -22,7 +25,14 @@ class Comment(models.Model):
     likes = models.SmallIntegerField(default=0)
     reports = models.PositiveSmallIntegerField(default=0)
 
+    def __str__(self):
+        return "{id}-{content}".format(id = self.id,content = self.content[0:20])
+
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return "{id}-{name}".format(id = self.id,name = self.name[0:20])
+
 
 
