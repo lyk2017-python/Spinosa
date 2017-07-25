e861d856e3429e60898aca8ffde0e6648913f4d3
Taner Karacayir <tanerkaracayir@yahoo.com>
2017-07-26 00:30:29 +0300

    Documentations added on models.py

--- spinosanews/news/models.py
+++ spinosanews/news/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 
 
 class News(models.Model):
+    ''' DOCUMENTATION
+    Title holds heading of the news, content holds the whole text,
+    source holds UFL of the page, slug keeps the title as an URL,
+    image gets addresses of images, publish_date and update date keeps when the entry has opened and modified,
+    likes and reports hold number of their counts and
+    categories as a M2M field, keeps and directs tags of entries '''
+
     title = models.CharField(max_length=200)
     content = models.TextField(unique=True)
     source = models.URLField()
@@ -18,6 +25,11 @@ class News(models.Model):
         return "{id}-{title}".format(id = self.id, title = self.title[0:20])
 
 class Comment(models.Model):
+    '''DOCUMENTATION
+    Content is main text of the comment, news gets id of the News and keeps it for direction,
+    publish_date and update date keeps when the comment has opened and modified,
+    likes and reports hold number of their counts.'''
+
     content = models.CharField(max_length=255)
     news = models.ForeignKey("News")
     publish_date = models.DateField(auto_now_add=True)
@@ -29,10 +41,12 @@ class Comment(models.Model):
         return "{id}-{content}".format(id = self.id,content = self.content[0:20])
 
 class Category(models.Model):
+    '''DOCUMENTATION
+    name holds the keywords of tags '''
+
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return "{id}-{name}".format(id = self.id,name = self.name[0:20])
-
 
 
