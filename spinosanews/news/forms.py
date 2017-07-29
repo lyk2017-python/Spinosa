7a44a59c2c7908b08dcabac5f7ed10edecf55bcb
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-07-29 11:57:28 +0300

    forms.py added. Creating form added to home page for creating news anonymously

---
+++ spinosanews/news/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from news.models import *
+
+class CreateNewsForm (forms.ModelForm):
+    class Meta:
+        model = News
+        exclude = [
+            "id",
+            "likes",
+            "reports",
+            "status",
+        ]
+
--- spinosanews/news/views.py
+++ spinosanews/news/views.py
@@ -1,12 +1,31 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views import generic
 from news.models import *
+from news.forms import *
 
 # Create your views here.
-class HomepageView(generic.ListView):
-    model = News
+class HomepageView(generic.CreateView):
+    form_class = CreateNewsForm
+    template_name = "news/news_list.html"
+    success_url = "/"
+
+    def get_status(self):
+        query = News.objects.filter(status=self.kwargs["status"])
+        if query.exists():
+            return query.get()
+        else:
+            raise Http404("Status error!!")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["newslist"] = News.objects.all()
+        return context
 
 class CategoryView(generic.DetailView):
+    def get_queryset(self):
+        return Category.objects.all()
+
     def get_queryset(self):
         return Category.objects.all()
 
